871608ce7b1d881ccc406975c12dbfc31b4137a9
hossein <hosteam01@gmail.com>
2023-01-05 01:45:56 +0330

    added plain suckless tools

---
+++ suckless/get.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/slock
+git clone https://git.suckless.org/slstatus
+git clone https://git.suckless.org/sent
+git clone https://git.suckless.org/farbfeld
+git clone https://git.suckless.org/tabbed
+git clone https://git.suckless.org/9base
