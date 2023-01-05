479112b815eb8e5b81183788d99fbeafdd7b022d
hossein <hosteam01@gmail.com>
2023-01-06 02:02:03 +0330

    added new colorscheme <orange>

---
+++ suckless/tabbed/colors/orange.h
@@ -0,0 +1,6 @@
+static const char* normbgcolor  = "#000000";
+static const char* normfgcolor  = "#eeeeec";
+static const char* selbgcolor   = "#000000";
+static const char* selfgcolor   = "#ff7700";
+static const char* urgfgcolor   = "#000000";
+static const char* urgbgcolor   = "#ff2222";
