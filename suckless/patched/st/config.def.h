f7c75c55420065b7aed6a9e680f21812ec9d5233
hossein <hosteam01@gmail.com>
2023-01-07 17:50:35 +0330

    changed font, disabled boxdraw

--- suckless/patched/st/config.def.h
+++ suckless/patched/st/config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-//static char *font = "Fira Code:pixelsize=22:antialias=true:autohint=true";
-static char *font = "Ubuntu Mono:pixelsize=27:antialias=true:autohint=true";
+static char *font = "Fira Code:pixelsize=22:antialias=true:autohint=true";
+//static char *font = "Ubuntu Mono:pixelsize=27:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 //	"SauceCodePro Nerd Font:pixelsize=21:antialias=true:autohint=true",
@@ -154,11 +154,11 @@ static unsigned int blinktimeout = 800;
  *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
  * 0: disable (render all U25XX glyphs normally from the font).
  */
-const int boxdraw = 1;
+const int boxdraw = 0;
 const int boxdraw_bold = 0;
 
 /* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
-const int boxdraw_braille = 1;
+const int boxdraw_braille = 0;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
