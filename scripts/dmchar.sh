25a7a72854b1e3e506bbde291399ac662bb3206f
hossein <hosteam01@gmail.com>
2023-02-21 18:23:00 +0330

    added `*` to have option for display *all* chars

--- scripts/dmchar.sh
+++ scripts/dmchar.sh
@@ -5,7 +5,7 @@ prompt=$(echo $0 | awk -F '/' '{print $NF;}')
 runner="dmenu -l 9 -p ${prompt} ${patched}"
 path="$HOME/.local/chars/"
 
-list=$(printf '%s\n' "unicode" "nerdfont" "font-awesome" | $runner) \
+list=$(printf '%s\n' "unicode" "nerdfont" "font-awesome" "*" | $runner) \
 	&& chosen=$(cut -d ';' -f1 ${path}/${list}.txt | ${runner}) \
 	&& notify-send -a ${prompt} "Copied to clipboard" "${chosen}" \
 	&& printf ${chosen} | sed "s/ .*//" \
