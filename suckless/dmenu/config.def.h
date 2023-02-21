68d8dfe2e7509fa1e45b0d660887741a789b38b3
hossein <hosteam01@gmail.com>
2023-02-21 23:06:31 +0330

    applied grid-4.9 patch

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -18,8 +18,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel]  =  { focus_fg,    focus_bg   },
 	[SchemeOut]  =  { sel_fg,      sel_bg     },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
