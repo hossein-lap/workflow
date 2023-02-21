522e421d4312b7dadf326024dda02bc7038b0bd1
hossein <hosteam01@gmail.com>
2023-02-21 22:35:54 +0330

    added new scheme

---
+++ suckless/dwm/colors/tokyo.h
@@ -0,0 +1,8 @@
+static const char normal_bg[]		= "#1b181f";
+//static const char normal_bg[]		= "#000000";
+static const char normal_fg[]		= "#71647f";
+static const char normal_br[]		= "#272833";
+//static const char focus_bg[]		= "#000000";
+static const char focus_bg[]		= "#242230";
+static const char focus_fg[]		= "#fd0000";
+static const char focus_br[]		= "#eeedf0";
