732bb1229d4c290657506eea5ad47ccb2341aa69
hossein <hosteam01@gmail.com>
2023-02-21 22:31:56 +0330

    changed font to *FiraCode Nerd Font*

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Source Code Pro:size=13"
+	"FiraCode Nerd Font:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
