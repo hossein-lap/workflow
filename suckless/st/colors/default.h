df75ee275eda3b33b0f84374218e8200b59bbcee
hossein <hosteam01@gmail.com>
2023-02-21 22:43:26 +0330

    added default colorscheme

---
+++ suckless/st/colors/default.h
@@ -0,0 +1,27 @@
+/* Colorscheme */
+static const char *colorname[] = {
+	/* 8 normal colors */
+	[0] = "black",    /* black   */
+	[1] = "red3",     /* red     */
+	[2] = "green3",   /* green   */
+	[3] = "yellow3",  /* yellow  */
+	[4] = "blue2",    /* blue    */
+	[5] = "magenta3", /* magenta */
+	[6] = "cyan3",    /* cyan    */
+	[7] = "gray90",   /* white   */
+	/* 8 bright colors */
+	[8]  = "gray50",  /* black   */
+	[9]  = "red",     /* red     */
+	[10] = "green",   /* green   */
+	[11] = "yellow",  /* yellow  */
+	[12] = "#5c5cff", /* blue    */
+	[13] = "magenta", /* magenta */
+	[14] = "cyan",    /* cyan    */
+	[15] = "white",   /* white   */
+	/* special colors */
+	[255] = 0,
+	[256] = "black", /* 256 -> bg */
+	[257] = "#cccccc", /* foreground */
+	[258] = "red", /* cursor background */
+	[259] = "black", /* cursor foreground */
+};
