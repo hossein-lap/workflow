3402eabe733e405ee60cc92ca49a1dd9c82f4d0f
hossein <hosteam01@gmail.com>
2023-02-21 22:30:48 +0330

    added accent color, lighter sel_bg

--- suckless/dmenu/colors/dracula.h
+++ suckless/dmenu/colors/dracula.h
@@ -5,10 +5,12 @@
 //#define ACCENTCOLOR "#50fa7b"
 #define ACCENTCOLOR "#f1fa8c"
 //#define ACCENTCOLOR "#8be9fd"
+//#define ACCENTCOLOR "#44475A"
+//#define ACCENTCOLOR "#282a36"
 
 static const char normal_fg[] = "#f8f8f2";
 static const char normal_bg[] = "#1e1f29";
 static const char focus_fg[]  = "#000000";
 static const char focus_bg[]  = ACCENTCOLOR;
 static const char sel_fg[]    = ACCENTCOLOR;
-static const char sel_bg[]    = "#282a36";
+static const char sel_bg[]    = "#44475A";
