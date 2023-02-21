a7dbaeb78bf53421e8f631bf6cb6034410aa7e4e
hossein <hosteam01@gmail.com>
2023-02-21 22:24:40 +0330

    added local ip

--- scripts/slsping.sh
+++ scripts/slsping.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 set -e
 
+null=♦
+
+if [ -z ${1} ]; then
+	dev=wlan0
+else
+	dev=$1
+fi
+
+lip=$(ip a \
+	| grep ${dev} \
+	| grep 'inet ' \
+	| awk '{print $2;}' \
+	| awk -F '/' '{print $1;}' \
+	| awk -F '.' '{print $NF;}')
+
+[ -z $lip ] && lip=$null
+
 mess=$(ping -c 1 1.1.1.1 \
 	| grep bytes \
 	| grep -v PING \
-	| awk -F '=' '{print $NF}' \
+	| awk -F '=' '{print $NF;}' \
 	| grep '^[0-9]')
 
-if [ $(echo $mess | awk '{print $NF}') -eq 'ms' ]; then
-	printf '%s' "${mess}" | awk '{print $1}'
+if [ $(echo $mess | awk '{print $NF}') = 'ms' ]; then
+	ping=$(echo ",${mess}" | awk '{print $1;}')
 else
-	printf '%s' '-'
+	ping=
 fi
 
-#sleep 2
+printf "${lip}${ping}"
+
+#sleep 2ping
