2ebfcfab305f8422e15fcc2140fe7ce1bab64887
hossein <hosteam01@gmail.com>
2023-02-21 22:19:34 +0330

    enabled sound
    renamed `dunstify` to `notify-send`

--- scripts/slsbattery.sh
+++ scripts/slsbattery.sh
@@ -12,20 +12,20 @@ args="-a $(echo "$0" | awk -F '/' '{print $NF;}') -i battery"
 if [ $status = 'Discharging' ] && [ $percent -lt 6 ]; then
 	time=$(echo "`date +%s` % 30" | bc)
 	if [ "$time" -eq '0' ]; then
-		#mpv --really-quiet ~/mus/sounds/notify/alert.wav &
-		dunstify $args -u critical "Baterry is dying: ${percent}%"
+		mpv --really-quiet ~/mus/sounds/notify/alert.wav &
+		notify-send $args -u critical "Baterry is dying: ${percent}%"
 	fi
 elif [ $status = 'Full' ]; then
 	time=$(echo "`date +%s` % 300" | bc)
 	if [ "$time" -eq '0' ]; then
-		#mpv --really-quiet ~/mus/sounds/notify/normal.wav &
-		dunstify $args -u normal "Baterry is full: ${percent}%"
+		mpv --really-quiet ~/mus/sounds/notify/normal.wav &
+		notify-send $args -u normal "Baterry is full: ${percent}%"
 	fi
 else
 	percent=$(acpi -b | awk -F '%' '{print $1;}' | awk '{print $NF"%"}')
 fi
 
-printf '%s' "${percent}"
+printf '%s' " ${percent} "
 #printf '%s\n' "$status"
 #elif [ $status = 'Full' ] && [ $percent = 100 ]; then
-	#dunstify $args -u low "Baterry is fully charged"
+	#notify-send $args -u low "Baterry is fully charged"
