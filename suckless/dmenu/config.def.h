a650d638fba1d233ece2118278fd13a14185763d
hossein <hosteam01@gmail.com>
2023-02-21 23:03:23 +0330

    applied borderoption-20200217-bf60a1e patch

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -25,7 +25,7 @@ static unsigned int lines      = 0;
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
-static const char worddelimiters[] = " ";
+static const char worddelimiters[] = " /?\"&[]";
 
 /* Size of the window border */
 static unsigned int border_width = 0;
