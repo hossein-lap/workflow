7af638b93a329d40d3d2e3bf2a22e2d70a6c0d8a
hossein <hosteam01@gmail.com>
2023-02-25 02:06:52 +0330

    switched sel_bg and sel_fg

--- suckless/dwm/colors/dracula.h
+++ suckless/dwm/colors/dracula.h
@@ -9,6 +9,7 @@
 static const char normal_bg[]	= "#1e1f29";
 static const char normal_br[]	= "#1e1f29";
 static const char normal_fg[]	= "#f8f8f2";
-static const char focus_bg[]	= ACCENTCOLOR;
+//static const char focus_bg[]	= "#3b4048";
+static const char focus_bg[]	= "#282a36";
 static const char focus_br[]	= ACCENTCOLOR;
-static const char focus_fg[]	= "#282a36";
+static const char focus_fg[]	= ACCENTCOLOR;
--- suckless/dwm/colors/solarized-dark.h
+++ suckless/dwm/colors/solarized-dark.h
@@ -1,6 +1,6 @@
-//#define ACCENTCOLOR "#b58900"
+#define ACCENTCOLOR "#b58900"
 //#define ACCENTCOLOR "#859900"
-#define ACCENTCOLOR "#268bd2"
+//#define ACCENTCOLOR "#268bd2"
 //#define ACCENTCOLOR "#6c71c4"
 //#define ACCENTCOLOR "#d33682"
 //#define ACCENTCOLOR "#eee8d5"
@@ -8,8 +8,8 @@
 static const char normal_fg[]		= "#fdf6e3";
 static const char normal_bg[]		= "#002b36";
 static const char normal_br[]		= "#002b36";
-static const char focus_fg[]		= "#002b36";
-static const char focus_bg[]		= ACCENTCOLOR;
+static const char focus_bg[]		= "#073642";
 static const char focus_br[]		= ACCENTCOLOR;
+static const char focus_fg[]		= ACCENTCOLOR;
 
 //static const char focus_bg[]		= "#073642";
