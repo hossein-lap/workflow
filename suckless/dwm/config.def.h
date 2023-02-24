024e62590da282c126bd849f1630e70355e28417
hossein <hosteam01@gmail.com>
2023-02-25 01:09:26 +0330

    added *Noto Color Emoji* font, changed gaps-size

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -13,14 +13,17 @@ static const char bdterm[]		= "lxterminal";
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 12;       /* horiz inner gap between windows */
+static const unsigned int gappoh    = 22;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode Nerd Font:size=14" };
+static const char *fonts[]          = {
+	"FiraCode Nerd Font:size=14",
+	"Noto Color Emoji:pixelsize=10:antialias=true:autohint=true"
+};
 static const char dmenufont[]       = { "FiraCode Nerd Font:size=14" };
 
 /* colorschemes */
