c5f1ecaaa70dfc8bd9bc016a1417c0246c5cd1ea
hossein <hosteam01@gmail.com>
2023-02-25 01:51:57 +0330

    added emoji, changed dmenu style to rendering them
    p.s: I have no Idea why with `-c` flag it takes
    so long to render emojis :/

--- scripts/dmchar.sh
+++ scripts/dmchar.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 prompt=$(echo $0 | awk -F '/' '{print $NF;}')
-patched='-c -bw 2 -g 2 -l 2'
+patched='-bw 0 -g 3 -l 20'
 runner="dmenu -l 9 -p ${prompt} ${patched}"
 path="$HOME/.local/chars/"
 
-list=$(printf '%s\n' "unicode" "nerdfont" "font-awesome" "*" | $runner) \
+list=$(printf '%s\n' "unicode" "emoji" "nerdfont" "font-awesome" "*" | $runner) \
 	&& chosen=$(cut -d ';' -f1 ${path}/${list}.txt | ${runner}) \
 	&& notify-send -a ${prompt} "Copied to clipboard" "${chosen}" \
 	&& printf ${chosen} | sed "s/ .*//" \
