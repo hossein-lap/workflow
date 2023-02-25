7881971a7698b999697b07dc872b0c36a6304f55
hossein <hosteam01@gmail.com>
2023-02-25 22:18:23 +0330

    updated source path

--- scripts/dmchar.sh
+++ scripts/dmchar.sh
@@ -3,7 +3,7 @@
 prompt=$(echo $0 | awk -F '/' '{print $NF;}')
 patched='-bw 0 -g 3 -l 20'
 runner="dmenu -l 9 -p ${prompt} ${patched}"
-path="$HOME/.local/chars/"
+path="$HOME/.local/hos/chars/"
 
 list=$(printf '%s\n' "unicode" "emoji" "nerdfont" "font-awesome" "*" | $runner) \
 	&& chosen=$(cut -d ';' -f1 ${path}/${list}.txt | ${runner}) \
