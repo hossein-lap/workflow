e65cfcb6ed27090a026b8391ce046f5b97378ec7
hossein <hosteam01@gmail.com>
2023-03-06 13:49:32 +0330

    fixed colors

--- suckless/dmenu/colors/fg/blue.h
+++ suckless/dmenu/colors/fg/blue.h
@@ -1,6 +1,6 @@
 static const char normal_fg[]	= "#ffffff";
 static const char normal_bg[]	= "#000000";
-static const char focus_fg[]	= "#FE8B00";
 static const char focus_bg[]	= "#000000";
-static const char sel_bg[]	= "#FE8B00";
+static const char focus_fg[]	= "#00aaff";
+static const char sel_bg[]	= "#00aaff";
 static const char sel_fg[]	= "#000000";
--- suckless/dmenu/colors/new-scheme.lua
+++ suckless/dmenu/colors/new-scheme.lua
@@ -1,18 +1,12 @@
 #!/bin/lua5.4
---  _  _
--- | || | github: hossein-lap
--- | __ | email:  hosteam01@gmail.com
--- |_||_| matrix: hosaidenpwd:matrix.org
---
 
 -- help function
 function help()
 	io.stderr:write("Create new colorscheme", "\n\n",
-			"Usage: ", arg[0], " <color-name> <hex-color>", "\n",
-			"\n", "\t",
-			"exmaple: \n\n\t", "$ ", arg[0], " cyan 00ffff \n",
-			"\n", "Run without argument for help page.",
-			"\n")
+		"Usage: ", arg[0], " <color-name> <hex-color>", "\n\n",
+		"\t", "exmaple: \n\n",
+		"\t", "$ ", arg[0], " cyan 00ffff \n\n",
+		"Run without argument for help page.", "\n")
 end
 
 -- check if has to args to show help message
@@ -39,35 +33,57 @@ end
 
 -- variable set
 local color = arg[2]
+local dcolor = "#000000"
+local lcolor = "#ffffff"
 
-local normal_fg = [[static const char normal_bg[]		= "#000000";]]
-local normal_bg = [[static const char normal_fg[]		= "#ffffff";]]
+local bg_normal_fg = [[static const char normal_bg[]	= "]] ..  dcolor .. [[";]]
+local bg_normal_bg = [[static const char normal_fg[]	= "]] ..  lcolor .. [[";]]
+local bg_focus_fg = [[static const char focus_fg[]	= "]] ..  dcolor .. [[";]]
+local bg_focus_bg = [[static const char focus_bg[]	= "#]] .. color .. [[";]]
+local bg_sel_fg = [[static const char sel_fg[]	= "#]] .. color .. [[";]]
+local bg_sel_bg = [[static const char sel_bg[]	= "]] ..  dcolor .. [[";]]
 
-local focus_bg = [[static const char focus_bg[]		= "#]] .. color .. [[";]]
-local focus_fg = [[static const char focus_fg[]		= "#000000";]]
+local fg_normal_fg	= [[static const char normal_bg[]	= "]] ..  dcolor .. [[";]]
+local fg_normal_bg	= [[static const char normal_fg[]	= "]] ..  lcolor .. [[";]]
+local fg_focus_fg	= [[static const char focus_fg[]	= "#]] .. color .. [[";]]
+local fg_focus_bg	= [[static const char focus_bg[]	= "]] ..  dcolor .. [[";]]
+local fg_sel_fg		= [[static const char sel_fg[]	= "]] ..  dcolor .. [[";]]
+local fg_sel_bg		= [[static const char sel_bg[]	= "#]] .. color .. [[";]]
 
-local sel_fg = [[static const char sel_fg[]		= "#000000";]]
-local sel_bg = [[static const char sel_bg[]		= "#]] .. color .. [[";]]
-
----- main
+-- main
 local fg = 'fg/' .. arg[1] .. '.h'
-
+local bg = 'bg/' .. arg[1] .. '.h'
 
 if file_exists(fg) then
 	io.write("Colors exists\n",
 			'\t', fg, '\n', '\t\27[0;3m',
 			'\27[0m\n', "Abroting\n")
-	return 1
+	if file_exists(bg) then
+		io.write("Colors exists\n",
+				'\t', bg, '\n', '\t\27[0;3m',
+				'\27[0m\n', "Abroting\n")
+		return 2
+	else
+		return 1
+	end
 else
+	local fg_file = assert(io.open (fg, 'w'))
+		fg_file:write(fg_normal_bg, '\n')
+		fg_file:write(fg_normal_fg, '\n')
+		fg_file:write(fg_focus_bg, '\n')
+		fg_file:write(fg_focus_fg, '\n')
+		fg_file:write(fg_sel_bg, '\n')
+		fg_file:write(fg_sel_fg, '\n')
+	fg_file:close()
+
+	local bg_file = assert(io.open (bg, 'w'))
+		bg_file:write(bg_normal_bg, '\n')
+		bg_file:write(bg_normal_fg, '\n')
+		bg_file:write(bg_focus_bg, '\n')
+		bg_file:write(bg_focus_fg, '\n')
+		bg_file:write(bg_sel_bg, '\n')
+		bg_file:write(bg_sel_fg, '\n')
+	bg_file:close()
 
 	print("Colorscheme created")
-
-	local fg_file = assert(io.open (fg, 'w'))
-		fg_file:write(normal_bg, '\n')
-		fg_file:write(normal_fg, '\n')
-		fg_file:write(focus_bg, '\n')
-		fg_file:write(focus_fg, '\n')
-		fg_file:write(sel_bg, '\n')
-		fg_file:write(sel_fg, '\n')
-	fg_file:close()
 end
