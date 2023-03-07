613cb209db61052618ea210f0a9ff736a7811f73
hossein <hosteam01@gmail.com>
2023-03-07 18:40:58 +0330

    no transparent bg

--- nvim/lua/plugins/dracula.lua
+++ nvim/lua/plugins/dracula.lua
@@ -28,7 +28,7 @@ dracula.setup({
 	-- show the '~' characters after the end of buffers
 	show_end_of_buffer = false, -- default false
 	-- use transparent background
-	transparent_bg = true, -- default false
+	transparent_bg = false, -- default false
 	-- set custom lualine background color
 --	lualine_bg_color = "#44475a", -- default nil
 	-- set italic comment
