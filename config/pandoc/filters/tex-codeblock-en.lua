b3325840c32d07f30b6ec5567dd0e1fb0ecef19c
hossein <hosteam01@gmail.com>
2023-03-11 04:48:51 +0330

    added nord theme, some fixes and reorgs applied, max tcb env on en-filters

--- config/pandoc/filters/tex-codeblock-en.lua
+++ config/pandoc/filters/tex-codeblock-en.lua
@@ -5,13 +5,12 @@ end
 
 -- Wrap code blocks in tcolorbox environments
 function CodeBlock(cb)
-	local cbft = cb.classes[1]
 	local codeblockbox = {
-		raw_tex '\\begin{quote}',
+--		raw_tex '\\begin{quote}',
 		raw_tex '\\begin{bbox}',
 			cb,
 		raw_tex '\\end{bbox}',
-		raw_tex '\\end{quote}',
+--		raw_tex '\\end{quote}',
 	}
 	local codeblock = {
 		raw_tex '\\begin{quote}',
@@ -19,6 +18,8 @@ function CodeBlock(cb)
 		raw_tex '\\end{quote}',
 	}
 
+	-- check if has a src_type
+	local cbft = cb.classes[1]
 	if cbft then
 		return codeblockbox
 	else
