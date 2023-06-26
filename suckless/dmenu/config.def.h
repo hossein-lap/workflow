8d7814de1f1985b0524f63bf44be6d42446d4415
hossein <hosteam01@gmail.com>
2023-06-26 17:24:42 +0330

    applied fuzzymatch-4.9 patch

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;		/* -b option; if 0, dmenu appears at bottom */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;	/* -c option; centers dmenu on screen */
 static int min_width = 600;	/* minimum width when centered */
 static int vertful = 0;		/* -vf option; if 1, no vertical padding */
