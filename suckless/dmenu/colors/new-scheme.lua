6f9a593910fee99bb1eb5f0ddea6f357a17a8e04
hossein <hosteam01@gmail.com>
2023-06-27 03:17:11 +0330

    added highlight-4.9 patch extra highlight

--- suckless/dmenu/colors/new-scheme.lua
+++ suckless/dmenu/colors/new-scheme.lua
@@ -1,12 +1,12 @@
 #!/bin/lua5.4
 
 -- help function
-function help()
+local function help()
 	io.stderr:write("Create new colorscheme", "\n\n",
-		"Usage: ", arg[0], " <color-name> <hex-color>", "\n\n",
-		"\t", "exmaple: \n\n",
-		"\t", "$ ", arg[0], " cyan 00ffff \n\n",
-		"Run without argument for help page.", "\n")
+	"Usage: ", arg[0], " <color-name> <hex-color>", "\n\n",
+	"\t", "exmaple: \n\n",
+	"\t", "$ ", arg[0], " cyan 00ffff \n\n",
+	"Run without argument for help page.", "\n")
 end
 
 -- check if has to args to show help message
@@ -21,34 +21,47 @@ if arg[2] == nil then
 end
 
 -- check for existing scheme
-function file_exists(name)
+local function file_exists(name)
 	local f = io.open(name, "r")
 	if f ~= nil then
-	   io.close(f)
-	   return true
+		io.close(f)
+		return true
 	else
-	   return false
+		return false
 	end
 end
 
 -- variable set
-local color = arg[2]
+--local color  = "ff7700"
+local color  = arg[2]
 local dcolor = "#000000"
 local lcolor = "#ffffff"
 
-local bg_normal_fg = [[static const char normal_bg[]	= "]] ..  dcolor .. [[";]]
-local bg_normal_bg = [[static const char normal_fg[]	= "]] ..  lcolor .. [[";]]
-local bg_focus_fg = [[static const char focus_fg[]	= "]] ..  dcolor .. [[";]]
-local bg_focus_bg = [[static const char focus_bg[]	= "#]] .. color .. [[";]]
-local bg_sel_fg = [[static const char sel_fg[]	= "#]] .. color .. [[";]]
-local bg_sel_bg = [[static const char sel_bg[]	= "]] ..  dcolor .. [[";]]
+local bg_scheme = {
+	'static const char normal_fg[]      = "'..lcolor..'";',
+	'static const char normal_bg[]      = "'..dcolor..'";',
+	'static const char focus_fg[]       = "'..dcolor..'";',
+	'static const char focus_bg[]       = "#'..color..'";',
+	'static const char sel_fg[]         = "#'..color..'";',
+	'static const char sel_bg[]         = "'..dcolor..'";',
+	'static const char high_sel_fg[]    = "'..dcolor..'";',
+	'static const char high_sel_bg[]    = "#'..color..'";',
+	'static const char high_normal_fg[] = "#'..color..'";',
+	'static const char high_normal_bg[] = "'..dcolor..'";',
+}
 
-local fg_normal_fg	= [[static const char normal_bg[]	= "]] ..  dcolor .. [[";]]
-local fg_normal_bg	= [[static const char normal_fg[]	= "]] ..  lcolor .. [[";]]
-local fg_focus_fg	= [[static const char focus_fg[]	= "#]] .. color .. [[";]]
-local fg_focus_bg	= [[static const char focus_bg[]	= "]] ..  dcolor .. [[";]]
-local fg_sel_fg		= [[static const char sel_fg[]	= "]] ..  dcolor .. [[";]]
-local fg_sel_bg		= [[static const char sel_bg[]	= "#]] .. color .. [[";]]
+local fg_scheme = {
+	'static const char normal_fg[]      = "'..lcolor..'";',
+	'static const char normal_bg[]      = "'..dcolor..'";',
+	'static const char focus_fg[]       = "#'..color..'";',
+	'static const char focus_bg[]       = "'..dcolor..'";',
+	'static const char sel_fg[]         = "'..dcolor..'";',
+	'static const char sel_bg[]         = "#'..color..'";',
+	'static const char high_sel_fg[]    = "#'..color..'";',
+	'static const char high_sel_bg[]    = "'..dcolor..'";',
+	'static const char high_normal_fg[] = "'..dcolor..'";',
+	'static const char high_normal_bg[] = "#'..color..'";',
+}
 
 -- main
 local fg = 'fg/' .. arg[1] .. '.h'
@@ -56,33 +69,27 @@ local bg = 'bg/' .. arg[1] .. '.h'
 
 if file_exists(fg) then
 	io.write("Colors exists:",
-			'\t', fg, '\t', '\27[0;3m',
-			'\27[0m', "[Abrot]\n")
+	'\t', fg, '\t', '\27[0;3m',
+	'\27[0m', "[Abrot]\n")
 	if file_exists(bg) then
 		io.write("Colors exists:",
-				'\t', bg, '\t', '\27[0;3m',
-				'\27[0m', "[Abrot]\n")
+		'\t', bg, '\t', '\27[0;3m',
+		'\27[0m', "[Abrot]\n")
 		return 2
 	else
 		return 1
 	end
 else
 	local fg_file = assert(io.open (fg, 'w'))
-		fg_file:write(fg_normal_bg, '\n')
-		fg_file:write(fg_normal_fg, '\n')
-		fg_file:write(fg_focus_bg, '\n')
-		fg_file:write(fg_focus_fg, '\n')
-		fg_file:write(fg_sel_bg, '\n')
-		fg_file:write(fg_sel_fg, '\n')
+	for _,v in ipairs(fg_scheme) do
+		fg_file:write(v, '\n')
+	end
 	fg_file:close()
 
 	local bg_file = assert(io.open (bg, 'w'))
-		bg_file:write(bg_normal_bg, '\n')
-		bg_file:write(bg_normal_fg, '\n')
-		bg_file:write(bg_focus_bg, '\n')
-		bg_file:write(bg_focus_fg, '\n')
-		bg_file:write(bg_sel_bg, '\n')
-		bg_file:write(bg_sel_fg, '\n')
+	for _,v in ipairs(bg_scheme) do
+		bg_file:write(v, '\n')
+	end
 	bg_file:close()
 
 	print(arg[1] .. ": colorscheme created")
